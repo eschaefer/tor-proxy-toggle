024d2d5a991c04da6c657bc358a2ed7c58487313
Eric Schaefer <omg@eric-schaefer.com>
2015-06-01 13:33:01 +0200

    Remove streaming proxy setting since it seems kinda unnnecessary.

--- bin/tor-proxy-toggle.sh
+++ bin/tor-proxy-toggle.sh
@@ -6,12 +6,9 @@ if [ -n "$e" ]; then
   echo "Turning on proxy"
   launchctl load ~/Library/LaunchAgents/homebrew.mxcl.tor.plist
   sudo networksetup -setsocksfirewallproxy wi-fi 127.0.0.1 9050
-  sudo networksetup -setstreamingproxy wi-fi 127.0.0.1 9050
-  sudo networksetup -setstreamingproxystate wi-fi on
   sudo networksetup -setsocksfirewallproxystate wi-fi on
 else
   echo "Turning off proxy"
-  sudo networksetup -setstreamingproxystate wi-fi off
   sudo networksetup -setsocksfirewallproxystate wi-fi off
   launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.tor.plist
 fi
